50dd6628e28e16f6b39efb060936f7df13307f4d
soleymannzhad <soleymannzhad@gmail.com>
2025-08-06 14:20:58 +0330

    new update

--- components/Journey.tsx
+++ components/Journey.tsx
@@ -1,9 +1,8 @@
 import {
-MdOutlineDateRange,
-PiStudentLight,
-BiBriefcaseAlt2
+  MdOutlineDateRange,
+  PiStudentLight,
+  BiBriefcaseAlt2,
 } from "./ui/icons";
-
 
 export default function Journey() {
   return (
@@ -19,129 +18,126 @@ export default function Journey() {
           {/* قسمت دو ستون این بخش  */}
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-5 pt-16 md:mx-40">
-
-
-{/* گرید سمت چپ  */}
+            {/* گرید سمت چپ  */}
             <div>
-                <div className="text-white flex items-center mb-8 text-2xl"><PiStudentLight/> Education </div>
+              <div className="text-white flex items-center mb-8 text-2xl font-bold">
+                <PiStudentLight /> Education
+              </div>
 
+              <div className="grid grid-cols-1">
+                <div className="relative border-colortheme2 border-l leading-7 hover:border-colortheme1  mt-8">
+                  <div className="pl-4">
+                    <span className="font-bold text-lg">
+                      Safaei High School (Mashhad – Layen)
+                    </span>
+                    <p className="text-gray-400">Diploma in Experimental Sciences</p>
+                    <span className="flex items-center text-colortheme2 text-sm mt-3 ">
+                      <span>
+                        <MdOutlineDateRange />
+                      </span>
+                      <span className="ml-2">2017 – 2020</span>
+                    </span>
+                  </div>
 
-                <div className="grid grid-cols-1">
-
-                    <div className="relative border-colortheme2 border-l leading-7 hover:border-colortheme1  mt-8">
-                        <div className="pl-4">
-                        <span className="font-bold text-lg">B.Sc. in Computer Science</span>
-                        <p className="text-gray-400">University of Tehran</p>
-                        <span className="flex items-center text-colortheme2 text-sm mt-3 ">
-                            <span><MdOutlineDateRange/></span> 
-                        <span className="ml-2">2020 – 2024</span>
-                        </span>
-                        </div>
-
-                        <span className="absolute -left-[6px] -top-[5px] bg-colortheme2 rounded-full w-3 h-3"></span>
-
-                    </div>
-
-
-
-                    <div className="relative border-colortheme2 border-l leading-7 hover:border-colortheme1  mt-8">
-                        <div className="pl-4">
-                        <span className="font-bold text-lg">B.Sc. in Computer Science</span>
-                        <p className="text-gray-400">University of Tehran</p>
-                        <span className="flex items-center text-colortheme2 text-sm mt-3 ">
-                            <span><MdOutlineDateRange/></span> 
-                        <span className="ml-2">2020 – 2024</span>
-                        </span>
-                        </div>
-
-                        <span className="absolute -left-[6px] -top-[5px] bg-colortheme2 rounded-full w-3 h-3"></span>
-
-                    </div>
-
-
-
-                    <div className="relative border-colortheme2 border-l leading-7 hover:border-colortheme1 mt-8">
-                        <div className="pl-4">
-                        <span className="font-bold text-lg">B.Sc. in Computer Science</span>
-                        <p className="text-gray-400">University of Tehran</p>
-                        <span className="flex items-center text-colortheme2 text-sm mt-3 ">
-                            <span><MdOutlineDateRange/></span> 
-                        <span className="ml-2">2020 – 2024</span>
-                        </span>
-                        </div>
-
-                        <span className="absolute -left-[6px] -top-[5px] bg-colortheme2 rounded-full w-3 h-3"></span>
-
-                    </div>
-
+                  <span className="absolute -left-[6px] -top-[5px] bg-colortheme2 rounded-full w-3 h-3"></span>
                 </div>
 
+                <div className="relative border-colortheme2 border-l leading-7 hover:border-colortheme1  mt-8">
+                  <div className="pl-4">
+                    <span className="font-bold text-lg">
+                      Zakaria Institute (Mashhad)
+                    </span>
+                    <p className="text-gray-400">Programming and Web Development</p>
+                    <span className="flex items-center text-colortheme2 text-sm mt-3 ">
+                      <span>
+                        <MdOutlineDateRange />
+                      </span>
+                      <span className="ml-2">2023 – 2024</span>
+                    </span>
+                  </div>
 
-                
-            </div>
-
-
-{/* گرید سمت راست  */}
-
-  <div>
-                <div className="text-white flex items-center mb-8 text-2xl"><BiBriefcaseAlt2/> Experience </div>
-
-
-                <div className="grid grid-cols-1">
-
-                    <div className="relative border-colortheme2 border-l leading-7 hover:border-colortheme1  mt-8">
-                        <div className="pl-4">
-                        <span className="font-bold text-lg">B.Sc. in Computer Science</span>
-                        <p className="text-gray-400">University of Tehran</p>
-                        <span className="flex items-center text-colortheme2 text-sm mt-3 ">
-                            <span><MdOutlineDateRange/></span> 
-                        <span className="ml-2">2020 – 2024</span>
-                        </span>
-                        </div>
-
-                        <span className="absolute -left-[6px] -top-[5px] bg-colortheme2 rounded-full w-3 h-3"></span>
-
-                    </div>
-
-
-
-                    <div className="relative border-colortheme2 border-l leading-7 hover:border-colortheme1  mt-8">
-                        <div className="pl-4">
-                        <span className="font-bold text-lg">B.Sc. in Computer Science</span>
-                        <p className="text-gray-400">University of Tehran</p>
-                        <span className="flex items-center text-colortheme2 text-sm mt-3 ">
-                            <span><MdOutlineDateRange/></span> 
-                        <span className="ml-2">2020 – 2024</span>
-                        </span>
-                        </div>
-
-                        <span className="absolute -left-[6px] -top-[5px] bg-colortheme2 rounded-full w-3 h-3"></span>
-
-                    </div>
-
-
-
-                    <div className="relative border-colortheme2 border-l leading-7 hover:border-colortheme1 mt-8">
-                        <div className="pl-4">
-                        <span className="font-bold text-lg">B.Sc. in Computer Science</span>
-                        <p className="text-gray-400">University of Tehran</p>
-                        <span className="flex items-center text-colortheme2 text-sm mt-3 ">
-                            <span><MdOutlineDateRange/></span> 
-                        <span className="ml-2">2020 – 2024</span>
-                        </span>
-                        </div>
-
-                        <span className="absolute -left-[6px] -top-[5px] bg-colortheme2 rounded-full w-3 h-3"></span>
-
-                    </div>
-
+                  <span className="absolute -left-[6px] -top-[5px] bg-colortheme2 rounded-full w-3 h-3"></span>
                 </div>
 
+                <div className="relative border-colortheme2 border-l leading-7 hover:border-colortheme1 mt-8">
+                  <div className="pl-4">
+                    <span className="font-bold text-lg">
+                     freeCodeCamp Organization (California)
+                    </span>
+                    <p className="text-gray-400">Front-End Development Specialization</p>
+                    <span className="flex items-center text-colortheme2 text-sm mt-3 ">
+                      <span>
+                        <MdOutlineDateRange />
+                      </span>
+                      <span className="ml-2">2024 – 2025</span>
+                    </span>
+                  </div>
 
-                
+                  <span className="absolute -left-[6px] -top-[5px] bg-colortheme2 rounded-full w-3 h-3"></span>
+                </div>
+              </div>
             </div>
-           
 
+            {/* گرید سمت راست  */}
+
+            <div>
+              <div className="text-white flex items-center mb-8 text-2xl font-bold">
+                <BiBriefcaseAlt2 /> Experience
+              </div>
+
+              <div className="grid grid-cols-1">
+                <div className="relative border-colortheme2 border-l leading-7 hover:border-colortheme1  mt-8">
+                  <div className="pl-4">
+                    <span className="font-bold text-lg">
+                      B.Sc. in Computer Science
+                    </span>
+                    <p className="text-gray-400">University of Tehran</p>
+                    <span className="flex items-center text-colortheme2 text-sm mt-3 ">
+                      <span>
+                        <MdOutlineDateRange />
+                      </span>
+                      <span className="ml-2">2020 – 2024</span>
+                    </span>
+                  </div>
+
+                  <span className="absolute -left-[6px] -top-[5px] bg-colortheme2 rounded-full w-3 h-3"></span>
+                </div>
+
+                <div className="relative border-colortheme2 border-l leading-7 hover:border-colortheme1  mt-8">
+                  <div className="pl-4">
+                    <span className="font-bold text-lg">
+                      B.Sc. in Computer Science
+                    </span>
+                    <p className="text-gray-400">University of Tehran</p>
+                    <span className="flex items-center text-colortheme2 text-sm mt-3 ">
+                      <span>
+                        <MdOutlineDateRange />
+                      </span>
+                      <span className="ml-2">2020 – 2024</span>
+                    </span>
+                  </div>
+
+                  <span className="absolute -left-[6px] -top-[5px] bg-colortheme2 rounded-full w-3 h-3"></span>
+                </div>
+
+                <div className="relative border-colortheme2 border-l leading-7 hover:border-colortheme1 mt-8">
+                  <div className="pl-4">
+                    <span className="font-bold text-lg">
+                      B.Sc. in Computer Science
+                    </span>
+                    <p className="text-gray-400">University of Tehran</p>
+                    <span className="flex items-center text-colortheme2 text-sm mt-3 ">
+                      <span>
+                        <MdOutlineDateRange />
+                      </span>
+                      <span className="ml-2">2020 – 2024</span>
+                    </span>
+                  </div>
+
+                  <span className="absolute -left-[6px] -top-[5px] bg-colortheme2 rounded-full w-3 h-3"></span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
